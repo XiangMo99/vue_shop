818747b44dadd455b8bfcc8bfe14fc49a16e1edf
tut <Xiang_Mo99@163.com>
2020-09-07 19:41:42 +0800

    777

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,10 @@ import Welcome from '../components/Welcome.vue'
 import User from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
-
+import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
+import GoodsList from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -20,7 +23,11 @@ const routes = [
       { path: '/welcome', component: Welcome },
       { path: '/users', component: User },
       { path: '/rights', component: Rights },
-      { path: '/roles', component: Roles }]
+      { path: '/roles', component: Roles },
+      { path: '/categories', component: Cate },
+      { path: '/params', component: Params },
+      { path: '/goods', component: GoodsList },
+      { path: '/goods/add', component: Add }]
   }
 ]
 
